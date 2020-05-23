8ef8ad932bc439bfbbbaf6f784b9ffa97772d524
Connor Holmes <connorholmes@Connors-MacBook-Pro-3.local>
2020-05-22 23:17:48 -0700

    Nodemon

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,6 +5,14 @@ app.get("/", function(req, resp) {
     resp.send("<h1>Hello, world!</h1>");
 });
 
+app.get("/contact", function(req, resp) {
+    resp.send("Contact me at connor5@gmail.com");
+});
+
+app.get("/about", function(req, resp) {
+    resp.send("My name is Connor");
+});
+
 app.listen(3000, function() {
     console.log('Server started on port 3000');
 });
