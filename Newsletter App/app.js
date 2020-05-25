75226e829206fb96185f6891c857172d9eb30705
Connor Holmes <connorholmes@Connors-MacBook-Pro-3.local>
2020-05-25 00:56:20 -0700

    Newsletter done and deployed

--- Newsletter App/app.js
+++ Newsletter App/app.js
@@ -37,6 +37,13 @@ app.post('/', function(req, res) {
     };
 
     const request = https.request(url, options, function(response) {
+        
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+        
         response.on("data", function(data) {
             console.log(JSON.parse(data));
         });
@@ -46,7 +53,11 @@ app.post('/', function(req, res) {
     request.end();
 });
 
-app.listen(3000, function() {
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
     console.log('Server is running on port 3000.');
 });
 
