91cc45368fc01fa89ea4b784ce33192a0b119bc7
Connor Holmes <connorholmes@Connors-MacBook-Pro-3.local>
2020-07-02 21:44:11 -0700

    Course finished

--- React/Keeper-App/src/components/app.js
+++ React/Keeper-App/src/components/app.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import CreateArea from "./createarea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+    
+    function addNote(note) {
+        setNotes(prevNotes => {
+            return [...prevNotes, note];
+        })
+    };
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((note, index) => {
+                return index !== id;
+            });
+        });
+    };
+    
     return <div>
         <Header />
-        <Note />
+        <CreateArea 
+            onAdd={addNote}
+        />
+        {notes.map((noteItem, index) => 
+                <Note
+                    key={index}
+                    id={index} 
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
+                />
+            )}
         <Footer />
     </div>
-}
+};
 
 export default App;
+
